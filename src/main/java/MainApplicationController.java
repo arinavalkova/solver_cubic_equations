274f116d36d97c87fc06dcf6b2ddb020eaa88e0e
Arina <sunny.qww@list.ru>
2020-09-08 18:46:55 +0700

    all work has done

--- src/main/java/MainApplicationController.java
+++ src/main/java/MainApplicationController.java
@@ -78,7 +78,7 @@ public class MainApplicationController
                 printRoots(rootsOfEquation);
             }
 
-            printGraphic(rootsOfEquation);
+            solvePrintGraphic(rootsOfEquation);
 
         } catch (NumberFormatException exception)
         {
@@ -101,7 +101,56 @@ public class MainApplicationController
         d = Double.parseDouble(fourthTextField.getText());
     }
 
-    private void printGraphic(ArrayList<Double> rootsOfEquation)
+    private void solvePrintGraphic(ArrayList<Double> rootsOfEquation)
+    {
+        double min = 9999999, max = 0;///////////////////
+
+        if(rootsOfEquation.size() > 1)
+        {
+            for(double currentRoot : rootsOfEquation)
+            {
+                if(currentRoot < min)
+                    min = currentRoot;
+                if(currentRoot > max)
+                    max = currentRoot;
+            }
+        }
+
+        if(rootsOfEquation.size() == 1)
+        {
+            printGraphicWithOneRoot(rootsOfEquation.get(0));
+        }
+        else if(rootsOfEquation.size() == 2 || rootsOfEquation.size() == 3)
+        {
+            printGraphicWithMultipleRoots(min, max);
+        }
+        else
+        {
+            System.out.println("Bad count of roots!");
+        }
+    }
+
+    private void printGraphicWithOneRoot(double root)
+    {
+        printGraphic((int) root - 5, (int) root + 5, 1);
+    }
+
+    private void printGraphicWithMultipleRoots(double minRoot, double maxRoot)
+    {
+        int step;
+        if((int)((maxRoot - minRoot) / 10) == 0)
+        {
+            step = 1;
+        }
+        else
+        {
+            step = (int) ((maxRoot - minRoot) / 10);
+        }
+        System.out.println(step);
+        printGraphic((int) minRoot - 3 * step, (int) maxRoot + 3 * step, step);
+    }
+
+    private void printGraphic(int leftScope, int rightScope, int step)
     {
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
@@ -111,7 +160,7 @@ public class MainApplicationController
         XYChart.Series series = new XYChart.Series();
         series.setName(a + " * x^3 + " + b + " * x^2 + " + c + " * x + " + d);
 
-        for(int i = -4; i <= 4; i += 1)//вычислить тут с какого i лучше по какое и шаг
+        for(int i = leftScope; i <= rightScope; i += step)//вычислить тут с какого i лучше по какое и шаг
         {
             series.getData().add(new XYChart.Data(i, cubicEquation(i)));
         }
@@ -169,22 +218,22 @@ public class MainApplicationController
 
         if(a > 0 && cubicEquation(0) > 0 || a < 0 && cubicEquation(0) < 0)
         {
-            rootsOfEquation.add(searchingLeftScope(0));
+            rootsOfEquation.add(searchingLeftScope(0, a > 0));
         }
         else
         {
-            rootsOfEquation.add(searchingRightScope(0));
+            rootsOfEquation.add(searchingRightScope(0, a > 0));
         }
         return rootsOfEquation;
     }
 
-    private double searchingLeftScope(double scope)
+    private double searchingLeftScope(double scope, boolean growUp)
     {
-        double leftScope, rightScope = scope, currentValue = 0;
+        double leftScope = 0, rightScope = scope, currentValue = scope;
 
         if(a > 0)
         {
-            while (cubicEquation(currentValue) > 0)
+            while (cubicEquation(currentValue) >= 0)
             {
                 rightScope = currentValue;
                 currentValue -= 1;
@@ -192,7 +241,7 @@ public class MainApplicationController
         }
         else
         {
-            while (cubicEquation(currentValue) < 0)
+            while (cubicEquation(currentValue) <= 0)
             {
                 rightScope = currentValue;
                 currentValue -= 1;
@@ -201,10 +250,10 @@ public class MainApplicationController
 
         leftScope = currentValue;
 
-        return searchingValueInScope(leftScope, rightScope);
+        return searchingValueInScope(leftScope, rightScope, growUp);
     }
 
-    private double searchingValueInScope(double leftScope, double rightScope)
+    private double searchingValueInScope(double leftScope, double rightScope, boolean growUp)
     {
         double currentValueOfEquation = cubicEquation(leftScope);
         if(Math.abs(currentValueOfEquation) <= EPSILON)
@@ -219,19 +268,19 @@ public class MainApplicationController
         if(Math.abs(currentValueOfEquation) <= EPSILON)
             return mid;
 
-        if(a > 0 && currentValueOfEquation > 0 || a < 0 && currentValueOfEquation < EPSILON)
-            return searchingValueInScope(leftScope, mid);
+        if(growUp && currentValueOfEquation > 0 || !growUp && currentValueOfEquation < EPSILON)
+            return searchingValueInScope(leftScope, mid, growUp);
         else
-            return searchingValueInScope(mid, rightScope);
+            return searchingValueInScope(mid, rightScope, growUp);
     }
 
-    private double searchingRightScope(double scope)
+    private double searchingRightScope(double scope, boolean growUp)
     {
-        double leftScope = scope, rightScope, currentValue = 0;
+        double leftScope = scope, rightScope, currentValue = scope;
 
         if(a > 0)
         {
-            while (cubicEquation(currentValue) < 0)
+            while (cubicEquation(currentValue) <= 0)
             {
                 leftScope = currentValue;
                 currentValue += 1;
@@ -239,7 +288,7 @@ public class MainApplicationController
         }
         else
         {
-            while (cubicEquation(currentValue) > 0)
+            while (cubicEquation(currentValue) >= 0)
             {
                 leftScope = currentValue;
                 currentValue += 1;
@@ -248,7 +297,7 @@ public class MainApplicationController
 
         rightScope = currentValue;
 
-        return searchingValueInScope(leftScope, rightScope);
+        return searchingValueInScope(leftScope, rightScope, growUp);
     }
 
     private ArrayList<Double> multipleRootsSearching()
@@ -262,22 +311,22 @@ public class MainApplicationController
         {
             if(a > 0)
             {
-                rootsOfEquation.add(searchingRightScope(Math.min(firstRoot, secondRoot)));
+                rootsOfEquation.add(searchingRightScope(Math.min(firstRoot, secondRoot), a > 0));
             }
             else
             {
-                rootsOfEquation.add(searchingLeftScope(Math.max(firstRoot, secondRoot)));
+                rootsOfEquation.add(searchingLeftScope(Math.max(firstRoot, secondRoot), a > 0));
             }
         }
         else if(cubicEquation(firstRoot) > EPSILON && cubicEquation(secondRoot) > EPSILON)
         {
             if(a > 0)
             {
-                rootsOfEquation.add(searchingLeftScope(Math.min(firstRoot, secondRoot)));
+                rootsOfEquation.add(searchingLeftScope(Math.min(firstRoot, secondRoot), a > 0));
             }
             else
             {
-                rootsOfEquation.add(searchingRightScope(Math.max(firstRoot, secondRoot)));
+                rootsOfEquation.add(searchingRightScope(Math.max(firstRoot, secondRoot), a > 0));
             }
         }
         else if(Math.abs(cubicEquation(Math.min(firstRoot, secondRoot))) < EPSILON)
@@ -285,7 +334,7 @@ public class MainApplicationController
             double root1 = Math.min(firstRoot, secondRoot);
             rootsOfEquation.add(root1);
 
-            double root2 = searchingRightScope(Math.min(firstRoot, secondRoot));
+            double root2 = searchingRightScope(Math.min(firstRoot, secondRoot), a > 0);
             if(root2 != root1)
             {
                 rootsOfEquation.add(root2);
@@ -296,11 +345,22 @@ public class MainApplicationController
             double root1 = Math.max(firstRoot, secondRoot);
             rootsOfEquation.add(root1);
 
-            double root2 = searchingLeftScope(Math.max(firstRoot, secondRoot));
+            double root2 = searchingLeftScope(Math.max(firstRoot, secondRoot), a > 0);
             if(root2 != root1)
             {
                 rootsOfEquation.add(root2);
             }
+        }
+        else if(cubicEquation(firstRoot) > EPSILON && cubicEquation(secondRoot) < - EPSILON ||
+                cubicEquation(firstRoot) < -EPSILON && cubicEquation(secondRoot) > EPSILON)
+        {
+            rootsOfEquation.add(searchingLeftScope(Math.min(firstRoot, secondRoot), a > 0));
+            rootsOfEquation.add(searchingValueInScope(Math.min(firstRoot, secondRoot), Math.max(firstRoot, secondRoot), a < 0));
+            rootsOfEquation.add(searchingRightScope(Math.max(firstRoot, secondRoot), a > 0));
+        }
+        else
+        {
+            rootsLabel.setText("Unknown way!");
         }
 
         return rootsOfEquation;
